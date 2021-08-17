b637582e3dc49e54bb7c06e798cb8021b8b27cd6
episohe <maybe_be@naver.com>
2021-08-18 01:36:09 +0900

    Ready for deploy

--- mindnote/wsgi.py
+++ mindnote/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mindNote project.
+WSGI config for mindnote project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mindNote.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mindnote.settings')
 
 application = get_wsgi_application()
